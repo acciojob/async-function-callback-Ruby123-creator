8d6d5741790ce5698871eca82c659dca3d5843d3
Ruby pal <rubyp20001@gmail.com>
2023-03-18 23:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
-
-//your JS code here. If required.
+let btn = document.getElementById("btn");
+ btn.addEventListener("click" ,getdata)
+ let div = document.getElementById("output")
+async function getdata(){
+	let response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    let data = await response.json();
+    console.log(data);
+	div.innerText = data.title;
+}
